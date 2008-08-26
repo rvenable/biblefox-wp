14cb95c7ac10966f755a0766d59ce4a55adffd2d
Richard Venable <richard@epicfox.com>
2008-08-26 04:39:26 +0000

    moving plan.php functionality to biblefox-plan.php

--- readingplan/trunk/biblefox-plan/biblefox-plan.php
+++ readingplan/trunk/biblefox-plan/biblefox-plan.php
@@ -11,7 +11,25 @@
 	include("bibletext.php");
 	connect_to_bible();
 
-	require_once("plan.php");
+	function create_plan_menu()
+	{
+	?>
+<h4>Create a Reading Plan</h4>
+<form action="biblefox-plan.php" method="post">
+<input type="hidden" name="hidden_field" value="Y">
+Which books?<br/>
+<textarea rows="5" cols="20" wrap="physical" name="books"></textarea><br/>
+How Fast?<br/>
+<input type="text" size="10" maxlength="40" name="num_chapters"> chapters per
+<select name="frequency">
+<option>day</option>
+<option>week</option>
+<option>month</option>
+</select>
+<input type="submit" />
+</form>
+<?php
+	}
 	
 	function get_sections($text, $size)
 	{
