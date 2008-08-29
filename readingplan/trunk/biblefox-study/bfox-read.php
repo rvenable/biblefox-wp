9ff017b551c9fee9ceaf9df4a2a9ccc08b15ba62
Richard Venable <richard@epicfox.com>
2008-08-29 06:40:50 +0000

    added translation pulldown to read

--- readingplan/trunk/biblefox-study/bfox-read.php
+++ readingplan/trunk/biblefox-study/bfox-read.php
@@ -1,6 +1,18 @@
 <?php
 
-	$version = 13;
+	global $wpdb;
+
+	// Get all enabled translations
+	$translations = $wpdb->get_results("SELECT id, short_name FROM " . BFOX_TRANSLATIONS_TABLE . " WHERE is_enabled = TRUE ORDER BY is_default DESC");
+	if (isset($_GET['trans_id']))
+	{
+		$trans_id = $_GET['trans_id'];
+	}
+	else
+	{
+		$trans_id = 13;
+	}
+
 	$reflistStr = trim($_GET['ref']);
 	if ($reflistStr == '')
 	{
@@ -12,19 +24,31 @@
 <form id="posts-filter" action="admin.php" method="get">
 <input type="hidden" name="page" value="<?php echo BFOX_READ_SUBPAGE; ?>" />
 <p id="post-search">
+<?php
+	$header = 'name="trans"';
+	echo '<select name="trans_id">';
+	foreach ($translations as $translation)
+	{
+		echo "<option value = \"$translation->id\"";
+		if ($translation->id == $trans_id) echo " selected";
+		echo ">$translation->short_name</option>";
+	}
+	echo '</select>';
+	?>
 <input type="text" id="post-search-input" name="ref" value="<?php echo $reflistStr; ?>" />
 <input type="submit" value="<?php _e('Search Bible', BFOX_DOMAIN); ?>" class="button" />
 </p>
 </form>
 
 <?php
+
 	if ($reflistStr != "")
 	{
 		$reflist = bfox_parse_reflist($reflistStr);
 		foreach ($reflist as $refStr)
 		{
 			$ref = bfox_parse_ref($refStr);
-			bfox_echo_scripture($version, $ref);
+			bfox_echo_scripture($trans_id, $ref);
 		}
 	}
 	else
@@ -32,7 +56,7 @@
 /*		$ref['book'] = $_GET['book'];
 		$ref['chapter1'] = $_GET['chapter1'];
 		$ref['verse1'] = $_GET['verse1'];*/
-		bfox_echo_scripture($version, $_GET);
+		bfox_echo_scripture($trans_id, $_GET);
 	}
 
 ?>
