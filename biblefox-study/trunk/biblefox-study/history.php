0e128aac4575eb9267aca170bf03314c6ce1ed63
Richard Venable <richard@epicfox.com>
2008-09-18 08:32:39 +0000

    moving read history table to be for entire website

--- biblefox-study/trunk/biblefox-study/history.php
+++ biblefox-study/trunk/biblefox-study/history.php
@@ -2,6 +2,13 @@
 
 	class History
 	{
+		private $table_name;
+
+		function History()
+		{
+			$this->table_name = BFOX_BASE_TABLE_PREFIX . 'read_history';
+		}
+
 		private function get_user_id()
 		{
 			global $user_ID;
@@ -15,7 +22,7 @@
 			// Note this function creates the table with dbDelta() which apparently has some pickiness
 			// See http://codex.wordpress.org/Creating_Tables_with_Plugins#Creating_or_Updating_the_Table
 			
-			$sql = "CREATE TABLE " . BFOX_TABLE_READ_HISTORY . " (
+			$sql = "CREATE TABLE $this->table_name (
 			id int,
 			user int,
 			verse_start int,
@@ -33,20 +40,19 @@
 		{
 			global $wpdb;
 			$refs = new BibleRefs;
-			$refs->push_sets($wpdb->get_results($wpdb->prepare("SELECT verse_start, verse_end FROM " . BFOX_TABLE_READ_HISTORY . " WHERE id = %d", $id), ARRAY_N));
+			$refs->push_sets($wpdb->get_results($wpdb->prepare("SELECT verse_start, verse_end FROM $this->table_name WHERE id = %d", $id), ARRAY_N));
 			return $refs;
 		}
 		
 		function update(BibleRefs $refs, $is_read = false)
 		{
 			global $wpdb;
-			$table_name = BFOX_TABLE_READ_HISTORY;
 			$id = 1;
 			
-			if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name)
+			if ($wpdb->get_var("SHOW TABLES LIKE '$this->table_name'") != $this->table_name)
 				$this->create_table();
 			else
-				$id = 1 + $wpdb->get_var("SELECT MAX(id) FROM $table_name");
+				$id = 1 + $wpdb->get_var("SELECT MAX(id) FROM $this->table_name");
 			
 			$user_id = $this->get_user_id();
 			
@@ -54,7 +60,7 @@
 			{
 				foreach ($refs->get_sets() as $unique_ids)
 				{
-					$insert = $wpdb->prepare("INSERT INTO $table_name (id, user, verse_start, verse_end, time, is_read) VALUES (%d, %d, %d, %d, NOW(), %d)", $id, $user_id, $unique_ids[0], $unique_ids[1], $is_read);
+					$insert = $wpdb->prepare("INSERT INTO $this->table_name (id, user, verse_start, verse_end, time, is_read) VALUES (%d, %d, %d, %d, NOW(), %d)", $id, $user_id, $unique_ids[0], $unique_ids[1], $is_read);
 					$wpdb->query($insert);
 				}
 			}
@@ -64,10 +70,9 @@
 		function get_refs_array($max = 1, $read = false)
 		{
 			global $wpdb;
-			$table_name = BFOX_TABLE_READ_HISTORY;
 			
 			$refs_array = array();
-			if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") == $table_name)
+			if ($wpdb->get_var("SHOW TABLES LIKE '$this->table_name'") == $this->table_name)
 			{
 				$user_id = $this->get_user_id();
 				if (0 < $user_id)
@@ -76,7 +81,7 @@
 					if ($read) $where_read = 'AND is_read = TRUE';
 					
 					// Get all the history ids for this user
-					$ids = $wpdb->get_col($wpdb->prepare("SELECT id FROM $table_name WHERE user = %d $where_read GROUP BY id ORDER BY time DESC", $user_id));
+					$ids = $wpdb->get_col($wpdb->prepare("SELECT id FROM $this->table_name WHERE user = %d $where_read GROUP BY id ORDER BY time DESC", $user_id));
 					
 					// Create an array of reference strings
 					if (0 < count($ids))
@@ -100,10 +105,9 @@
 		function get_ref_history(BibleRefs $refs, $max = 1, $read = false)
 		{
 			global $wpdb;
-			$table_name = BFOX_TABLE_READ_HISTORY;
 			
 			$ids = array();
-			if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") == $table_name)
+			if ($wpdb->get_var("SHOW TABLES LIKE '$this->table_name'") == $this->table_name)
 			{
 				$user_id = $this->get_user_id();
 				if (0 < $user_id)
@@ -111,10 +115,10 @@
 					// Add a where clause for is_read
 					if ($read) $where_read = 'AND is_read = TRUE';
 					
-					$where_ref = 'AND ' . $refs->sql_where2("$table_name.verse_start", "$table_name.verse_end");
+					$where_ref = 'AND ' . $refs->sql_where2("$this->table_name.verse_start", "$this->table_name.verse_end");
 					
 					// Get all the history ids for this user
-					$ids = $wpdb->get_col($wpdb->prepare("SELECT id FROM $table_name WHERE user = %d $where_read $where_ref GROUP BY id ORDER BY time DESC LIMIT %d", $user_id, $max));
+					$ids = $wpdb->get_col($wpdb->prepare("SELECT id FROM $this->table_name WHERE user = %d $where_read $where_ref GROUP BY id ORDER BY time DESC LIMIT %d", $user_id, $max));
 				}
 			}
 			
@@ -124,8 +128,7 @@
 		function get_date_for_id($id)
 		{
 			global $wpdb;
-			$table_name = BFOX_TABLE_READ_HISTORY;
-			return $wpdb->get_var($wpdb->prepare("SELECT DATE(time) FROM $table_name WHERE id = %d LIMIT 1", $id));
+			return $wpdb->get_var($wpdb->prepare("SELECT DATE(time) FROM $this->table_name WHERE id = %d LIMIT 1", $id));
 		}
 		
 		function get_special_url($read)
