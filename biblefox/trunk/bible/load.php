9c0cec673b221ccc0d8cc1528aeb1cdecb17d42e
Richard Venable <richard@epicfox.com>
2009-04-15 22:07:07 +0000

    ASSIGNED - bug 65: Reorganize files into directories http://dev.may26.org/bugzilla/show_bug.cgi?id=65
    deleting read.php

--- biblefox/trunk/bible/load.php
+++ biblefox/trunk/bible/load.php
@@ -1,15 +1,6 @@
 <?php
 
 define(BFOX_BIBLE_DIR, dirname(__FILE__));
-
-// TODO3: get rid of old bfox-read page
-define(BFOX_READ_SUBPAGE, 'bfox-read');
-function bfox_read()
-{
-	require_once('read.php');
-	bfox_read_menu();
-}
-
 define(BFOX_BIBLE_SUBPAGE, 'bfox-bible');
 
 function bfox_bible_page_load()
@@ -29,7 +20,6 @@ function bfox_bible_menu()
 	add_menu_page('Study the Bible', 'The Bible', 0, BFOX_BIBLE_SUBPAGE, 'bfox_bible_page');
 	add_submenu_page(BFOX_BIBLE_SUBPAGE, 'Bible', 'Bible', 0, BFOX_BIBLE_SUBPAGE, 'bfox_bible_page');
 	add_action('load-' . get_plugin_page_hookname(BFOX_BIBLE_SUBPAGE, BFOX_BIBLE_SUBPAGE), 'bfox_bible_page_load');
-	add_submenu_page(BFOX_BIBLE_SUBPAGE, 'Advanced Reading Pane', 'Read', 0, BFOX_READ_SUBPAGE, 'bfox_read');
 }
 
 function bfox_bible_init()
