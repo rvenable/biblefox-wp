9f7f36a24ff126274e5dcb34b90c97723d360e69
Richard Venable <richard@epicfox.com>
2009-06-20 21:22:33 +0000

    ASSIGNED - bug 71: Biblefox 1.0 http://dev.may26.org/bugzilla/show_bug.cgi?id=71
    version 0.4.1

--- biblefox/trunk/admin/admin_toolbox.php
+++ biblefox/trunk/admin/admin_toolbox.php
@@ -419,7 +419,7 @@ class BfoxMainToolbox extends BfoxToolBox
 	public function refresh_all_blog_posts() {
 		// The function expires after a certain version
 		$ver_num = self::version_to_int(get_site_option(Biblefox::option_version));
-		if ($ver_num <= self::version_to_int('0.4.0')) {
+		if ($ver_num <= self::version_to_int('0.4.1')) {
 
 			$blogs = get_blog_list(0, 'all');
 			foreach ($blogs as $blog_arr) {
