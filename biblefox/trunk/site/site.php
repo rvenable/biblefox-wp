ec1a44f549562422f6460a09889ae3cd16ef37e1
Richard Venable <richard@epicfox.com>
2009-06-22 21:29:29 +0000

    ASSIGNED - bug 71: Biblefox 1.0 http://dev.may26.org/bugzilla/show_bug.cgi?id=71
    BiblefoxSite class cleaning

--- biblefox/trunk/site/site.php
+++ biblefox/trunk/site/site.php
@@ -14,24 +14,6 @@ class BiblefoxSite {
 
 	private static $active_page = '';
 
-	/**
-	 * Returns the bible study blogs for a given user
-	 *
-	 * Should be used in place of get_blogs_of_user() because the main biblefox.com blog should not count
-	 *
-	 * @param integer $user_id
-	 * @return array of blogs (see get_blogs_of_user())
-	 */
-	public static function get_bible_study_blogs($user_id) {
-		// Get the blogs for the user
-		$blogs = get_blogs_of_user($user_id);
-
-		// The main biblefox blog does not count as a bible study blog
-		unset($blogs[1]);
-
-		return $blogs;
-	}
-
 	public static function new_blog_settings($blog_id, $user_id) {
 		global $wpdb;
 
@@ -247,10 +229,22 @@ class BiblefoxSite {
 		<?php
 	}
 
+	/**
+	 * Filter function for changine the email from name to Biblefox
+	 *
+	 * @param string $from_name
+	 * @return string
+	 */
+	function wp_mail_from_name($from_name) {
+		if ('WordPress' == $from_name) $from_name = 'Biblefox';
+		return $from_name;
+	}
+
 	public static function init() {
 		add_filter('query_vars', 'BiblefoxSite::query_vars');
 		add_action('parse_request', 'BiblefoxSite::parse_request');
 		add_action('wpmu_new_blog', 'BiblefoxSite::new_blog_settings', 10, 2);
+		add_filter('wp_mail_from_name', 'BiblefoxSite::wp_mail_from_name');
 		wp_deregister_style('login');
 		BfoxUtility::register_style('login', 'site/login.css');
 
@@ -273,41 +267,5 @@ class BiblefoxSite {
 }
 add_action('init', 'BiblefoxSite::init');
 add_filter('wpabar_defaults', 'BiblefoxSite::wpabar_defaults');
-
-
-/**
- * Filter function for changine the email from name to Biblefox
- *
- * @param string $from_name
- * @return string
- */
-function bfox_wp_mail_from_name($from_name)
-{
-	if ('WordPress' == $from_name) $from_name = 'Biblefox';
-	return $from_name;
-}
-add_filter('wp_mail_from_name', 'bfox_wp_mail_from_name');
-
-/**
- * Filter function for allowing page titles to be used in the exclude parameter passed to wp_list_pages()
- *
- * TODO2: See if we still need this function
- *
- * @param unknown_type $excludes
- * @return unknown
- */
-function bfox_list_pages_excludes($excludes)
-{
-	global $wpdb;
-
-	// Convert any string title excludes to a post id
-	// wpdb query from get_page_by_title()
-	foreach ($excludes as &$exclude)
-		if (!is_integer($exclude))
-			$exclude = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE post_title = %s AND post_type='page'", $exclude ));
-
-	return $excludes;
-}
-add_filter('wp_list_pages_excludes', 'bfox_list_pages_excludes');
 
 ?>
