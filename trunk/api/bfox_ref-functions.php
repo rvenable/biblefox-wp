06a7049fdeb953a61410124e0bb46387a3ec1c50
Richard Venable <richard@epicfox.com>
2011-10-09 17:17:10 -0400

    Added an API directory, and began moving some functions to it

---
+++ trunk/api/bfox_ref-functions.php
@@ -0,0 +1,140 @@
+<?php
+
+function bfox_ref_url($ref_str, $post_id = 0) {
+	if ($post_id) $bible_url = get_post_permalink($post_id);
+	else $bible_url = get_post_type_archive_link('bfox_tool');
+
+	return add_query_arg('ref', urlencode(strtolower($ref_str)), $bible_url);
+}
+
+/**
+ * Getter/Setter for the active instance of BfoxRef
+ *
+ * @param BfoxRef $ref
+ * @return BfoxRef
+ */
+function bfox_active_ref(BfoxRef $ref = null) {
+	global $_bfox_ref;
+	if (!is_null($ref)) $_bfox_ref = $ref;
+	if (!isset($_bfox_ref)) $_bfox_ref = new BfoxRef;
+	return $_bfox_ref;
+}
+
+function bfox_active_ref_str($format = '') {
+	$ref = bfox_ref();
+	return $ref->get_string($format);
+}
+
+/**
+ * Fixes a bible ref link options array so that it has a ref_str if it doesn't already
+ *
+ * @param $options
+ */
+function bfox_fix_ref_link_options(&$options) {
+	// If there is no ref_str, try to get it from $ref->get_string($name)
+	if (empty($options['ref_str']) && isset($options['ref']) && is_a($options['ref'], 'BfoxRef') && $options['ref']->is_valid())
+		$options['ref_str'] = $options['ref']->get_string($options['name']);
+}
+
+/**
+ * Creates a link from an array specifying bible ref link options
+ *
+ * Used to create links by bfox_ref_bible_link() and bfox_ref_blog_link()
+ *
+ * @param array $options
+ * @return string
+ */
+function bfox_ref_link_from_options($options = array()) {
+	extract($options);
+	$link = '';
+
+	// Only create a link if we actually have a ref_str
+	if (!empty($ref_str)) {
+		// If there is no text, use the ref_str
+		if (empty($text)) $text = $ref_str;
+
+		// If there is no href, get it from the bfox_ref_bible_url function
+		if (!isset($attrs['href'])) bfox_ref_bible_url($ref_str);
+
+		// Add the bible-ref class
+		if (!isset($disable_tooltip)) {
+			if (!empty($attrs['class'])) $attrs['class'] .= ' ';
+			$attrs['class'] .= 'bible-tip bible-tip-' . urlencode(str_replace(' ', '_', strtolower($ref_str)));
+		}
+
+		$attr_str = '';
+		foreach ($attrs as $attr => $value) $attr_str .= " $attr='$value'";
+
+		$link = "<a$attr_str>$text</a>";
+	}
+
+	return $link;
+}
+
+/**
+ * Returns a URL to the external Bible reader of choice for a given Bible Ref
+ *
+ * Should be used whenever we want to link to the Bible page, as opposed to the Bible archive
+ *
+ * @param string $ref_str
+ * @return string
+ */
+function bfox_ref_bible_url($ref_str) {
+	return bfox_ref_url($ref_str);
+}
+
+/**
+ * Returns a link to the external Bible reader of choice for a given Bible Ref
+ *
+ * Should be used whenever we want to link to the Bible page, as opposed to the Bible archive
+ *
+ * @param array $options
+ * @return string
+ */
+function bfox_ref_bible_link($options) {
+	bfox_fix_ref_link_options($options);
+
+	// If there is no href, get it from the bfox_ref_bible_url() function
+	if (!isset($options['attrs']['href'])) $options['attrs']['href'] = bfox_ref_bible_url($options['ref_str']);
+
+	return bfox_ref_link_from_options($options);
+}
+
+/**
+ * Replaces bible references with bible links in a given html string
+ * @param string $content
+ * @return string
+ */
+function bfox_ref_replace_html($content, $callback = 'bfox_ref_replace_html_cb') {
+	return BfoxRefParser::simple_html($content, null, $callback);
+}
+	function bfox_ref_replace_html_cb($text, $ref) {
+		return bfox_ref_bible_link(array(
+			'ref' => $ref,
+			'text' => $text
+		));
+	}
+
+/**
+ * Returns a BfoxRef for the given tag string
+ *
+ * @param string $tag
+ * @return BfoxRef
+ */
+function bfox_ref_from_tag($tag) {
+	return BfoxRefParser::simple($tag);
+}
+
+/**
+ * Returns a BfoxRef for the given content string
+ *
+ * @param string $content
+ * @return BfoxRef
+ */
+function bfox_ref_from_content($content) {
+	$ref = new BfoxRef;
+	BfoxRefParser::simple_html($content, $ref);
+	return $ref;
+}
+
+?>
--- trunk/biblefox-for-wordpress.php
+++ trunk/biblefox-for-wordpress.php
@@ -37,153 +37,20 @@ Text Domain: bfox
 define('BFOX_VERSION', '0.8.3');
 define('BFOX_DIR', dirname(__FILE__));
 define('BFOX_REF_DIR', BFOX_DIR . '/external/biblefox-ref');
+define('BFOX_API_DIR', BFOX_DIR . '/api');
 define('BFOX_URL', WP_PLUGIN_URL . '/biblefox-for-wordpress');
 define('BFOX_PLANS_URL', BFOX_URL . '/reading-plans');
 
 require_once BFOX_REF_DIR . '/biblefox-ref.php';
 
+require_once BFOX_API_DIR . '/bfox_ref-functions.php';
+
 require_once BFOX_DIR . '/biblefox-blog/biblefox-blog.php';
 
+// TODO: these need to be moved into API
 require_once BFOX_DIR . '/bibletext.php';
 require_once BFOX_DIR . '/translations.php';
 require_once BFOX_DIR . '/iframe.php';
-
-/**
- * Getter/Setter for the active instance of BfoxRef
- *
- * @param BfoxRef $ref
- * @return BfoxRef
- */
-function bfox_active_ref(BfoxRef $ref = null) {
-	global $_bfox_active_ref;
-	if (!is_null($ref)) $_bfox_active_ref = $ref;
-	if (!isset($_bfox_active_ref)) $_bfox_active_ref = new BfoxRef;
-	return $_bfox_active_ref;
-}
-
-function bfox_active_ref_str($format = '') {
-	$ref = bfox_active_ref();
-	return $ref->get_string($format);
-}
-
-/**
- * Fixes a bible ref link options array so that it has a ref_str if it doesn't already
- *
- * @param $options
- */
-function bfox_fix_ref_link_options(&$options) {
-	// If there is no ref_str, try to get it from $ref->get_string($name)
-	if (empty($options['ref_str']) && isset($options['ref']) && is_a($options['ref'], 'BfoxRef') && $options['ref']->is_valid())
-		$options['ref_str'] = $options['ref']->get_string($options['name']);
-}
-
-/**
- * Creates a link from an array specifying bible ref link options
- *
- * Used to create links by bfox_ref_bible_link() and bfox_ref_blog_link()
- *
- * @param array $options
- * @return string
- */
-function bfox_ref_link_from_options($options = array()) {
-	extract($options);
-	$link = '';
-
-	// Only create a link if we actually have a ref_str
-	if (!empty($ref_str)) {
-		// If there is no text, use the ref_str
-		if (empty($text)) $text = $ref_str;
-
-		// If there is no href, get it from the bfox_ref_bible_url function
-		if (!isset($attrs['href'])) bfox_ref_bible_url($ref_str);
-
-		// Add the bible-ref class
-		if (!isset($disable_tooltip)) {
-			if (!empty($attrs['class'])) $attrs['class'] .= ' ';
-			$attrs['class'] .= 'bible-tip bible-tip-' . urlencode(str_replace(' ', '_', strtolower($ref_str)));
-		}
-
-		$attr_str = '';
-		foreach ($attrs as $attr => $value) $attr_str .= " $attr='$value'";
-
-		$link = "<a$attr_str>$text</a>";
-	}
-
-	return $link;
-}
-
-function bfox_ref_url($ref_str, $post_id = 0) {
-	if ($post_id) $bible_url = get_post_permalink($post_id);
-	else $bible_url = get_post_type_archive_link('bfox_tool');
-
-	return add_query_arg('ref', urlencode(strtolower($ref_str)), $bible_url);
-}
-
-/**
- * Returns a URL to the external Bible reader of choice for a given Bible Ref
- *
- * Should be used whenever we want to link to the Bible page, as opposed to the Bible archive
- *
- * @param string $ref_str
- * @return string
- */
-function bfox_ref_bible_url($ref_str) {
-	return bfox_ref_url($ref_str);
-}
-
-/**
- * Returns a link to the external Bible reader of choice for a given Bible Ref
- *
- * Should be used whenever we want to link to the Bible page, as opposed to the Bible archive
- *
- * @param array $options
- * @return string
- */
-function bfox_ref_bible_link($options) {
-	bfox_fix_ref_link_options($options);
-
-	// If there is no href, get it from the bfox_ref_bible_url() function
-	if (!isset($options['attrs']['href'])) $options['attrs']['href'] = bfox_ref_bible_url($options['ref_str']);
-
-	return bfox_ref_link_from_options($options);
-}
-
-/**
- * Replaces bible references with bible links in a given html string
- * @param string $content
- * @return string
- */
-function bfox_ref_replace_html($content) {
-	return BfoxRefParser::simple_html($content, null, 'bfox_ref_replace_html_cb');
-}
-	function bfox_ref_replace_html_cb($text, $ref) {
-		return bfox_ref_bible_link(array(
-			'ref' => $ref,
-			'text' => $text
-		));
-	}
-
-/**
- * Returns a BfoxRef for the given tag string
- *
- * @param string $tag
- * @return BfoxRef
- */
-function bfox_ref_from_tag($tag) {
-	return BfoxRefParser::simple($tag);
-}
-
-/**
- * Returns a BfoxRef for the given content string
- *
- * @param string $content
- * @return BfoxRef
- */
-function bfox_ref_from_content($content) {
-	$ref = new BfoxRef;
-	BfoxRefParser::simple_html($content, $ref);
-	return $ref;
-}
 
 /**
  * Checks to see if we are requesting tooltip content (ie. by an AJAX call), returns the content, and exits
