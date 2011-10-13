7c5e9e6a9aec4837058ef50b5d950be38a791be3
Richard Venable <richard@epicfox.com>
2011-10-13 17:37:21 -0400

    Moved bfox_ajax_send_bible_text() to main file and deleted bibletext.php

--- trunk/biblefox-for-wordpress.php
+++ trunk/biblefox-for-wordpress.php
@@ -52,7 +52,6 @@ require_once BFOX_API_DIR . '/bfox_tool-template.php';
 require_once BFOX_DIR . '/biblefox-blog/biblefox-blog.php';
 
 // TODO: these need to be moved into API
-require_once BFOX_DIR . '/bibletext.php';
 require_once BFOX_DIR . '/translations.php';
 require_once BFOX_DIR . '/iframe.php';
 
@@ -78,6 +77,26 @@ function bfox_check_for_tooltip() {
 	}
 }
 add_action('init', 'bfox_check_for_tooltip', 1000);
+
+/*
+ AJAX function for sending the bible text
+ */
+function bfox_ajax_send_bible_text() {
+	sleep(1);
+
+	set_bfox_ref(new BfoxRef($_POST['ref_str']));
+
+	ob_start();
+
+	load_bfox_template('admin-bfox_tool');
+
+	$content = ob_get_clean();
+	$content = addslashes(str_replace("\n", '', $content));
+
+	$script = "bfox_quick_view_loaded('$ref_str', '$content');";
+	die($script);
+}
+add_action('wp_ajax_bfox_ajax_send_bible_text', 'bfox_ajax_send_bible_text');
 
 /**
  * Returns a path for a Biblefox theme template file, first trying to load from the theme, then from the plugin
